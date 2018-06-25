74e8e300dd01aebdf1b75ab60255149ed2ec93c4
André Paes <andrepaes@MacBook-Pro-de-Andre.local>
2018-06-25 03:25:16 -0300

    ..

--- principal.py
+++ principal.py
@@ -1,9 +1,6 @@
 import googlemaps
-from datetime import datetime
-import json
 import sys
 import numpy as np
-import time
 
 gmaps = []
 lista_keys = open("google_keys.txt")
@@ -23,15 +20,15 @@ else:
     nome_cidade = raw_input("Digite o nome da cidade:")
     tamanho_matriz_x = int(sys.argv[1])
     tamanho_matriz_y = int(sys.argv[2])
-    fator_x = float(sys.argv[1])/10000
-    fator_y = float(sys.argv[2])/10000
+    fator_x = float(sys.argv[1])/100000
+    fator_y = float(sys.argv[2])/100000
     tamanho_total = tamanho_matriz_x * tamanho_matriz_y
     print tamanho_total
     quantidade_suportada = float(quantidade_total_keys * 2000)
     ratio_coordenadas = round(tamanho_total / quantidade_suportada)
     if(ratio_coordenadas == 0):
         ratio_coordenadas = 1
-    ratio_coordenadas=3
+    ratio_coordenadas=10
     print ratio_coordenadas
 
 
@@ -45,8 +42,8 @@ lat = geocode_result[0]["geometry"]["location"]["lat"]
 lng = geocode_result[0]["geometry"]["location"]["lng"]
 
 nome_arquivo = "" + nome_cidade + ".mat"
-recuo_lat = lat + abs(fator_x * tamanho_matriz_x)
-recuo_lng = lng - abs(fator_y * tamanho_matriz_y)
+recuo_lat = lat + abs(fator_x * (tamanho_matriz_x / 2))
+recuo_lng = lng - abs(fator_y * (tamanho_matriz_y / 2))
 aux_lng = recuo_lng
 arquivo = open(nome_arquivo,"w")
 cabecalho = "c " + str(tamanho_matriz_x) + " " + str(tamanho_matriz_y) + "\n"
@@ -55,27 +52,35 @@ arquivo.close()
 matriz_gerada = []
 linha = []
 contador_keys = 0
-key_number = 0
+key_number = 1
 quantidade_ciclos = 0
 alturas = []
-for i in range(tamanho_matriz_x):
+i = 0
+while(i < tamanho_matriz_x):
     alturas.append([])
+    i += 1
+
+i = 0
 # zera toda a matriz com excecao dos pontos escolhidos para calcular altura
-for i in range(tamanho_matriz_x):
+while(i < tamanho_matriz_x):
     linha = []
     recuo_lng = aux_lng
-    for j in range(tamanho_matriz_y):
-        print recuo_lat,recuo_lng
+    j = 0
+    while (j < tamanho_matriz_y):
         if((quantidade_ciclos % ratio_coordenadas) == 0):
             try:
                 altura = gmaps[key_number].elevation((recuo_lat, recuo_lng))
+                print "antes"
                 alturas[i].append(altura[0]["elevation"])
+                print i,j
+                print "depois"
             except googlemaps.exceptions.Timeout:
                 print "trocando chave"
                 key_number += 1
                 if(key_number >= quantidade_total_keys):
                     print "Quantidade de chaves insuficiente"
                     sys.exit()
+
                 continue
         else:
             altura[0]["elevation"] = 0
@@ -83,44 +88,63 @@ for i in range(tamanho_matriz_x):
         if(altura[0]["elevation"] < 0):
             altura[0]["elevation"] = 0
 
-        print altura[0]["elevation"]
         linha.append(altura[0]["elevation"])
         recuo_lng += abs(fator_y)
         quantidade_ciclos += 1
+        j += 1
     matriz_gerada.append(linha)
     recuo_lat -= abs(fator_x)
+    i += 1
 
+print "Gerando matriz"
 
+i = 0
 quantidade_ciclos = 0
+print alturas
 flag = 0
 # popular os valores da matriz que nao foram preenchidos no processo anterior
 if(ratio_coordenadas > 1):
-    for i in range(tamanho_matriz_x):
+    while(i < tamanho_matriz_x):
         # variavel que percorre a matriz de alturas
         j_alturas = 1
-        for j in range(tamanho_matriz_y):
+
+        j = 0
+        while(j < tamanho_matriz_y):
+            print i,j
             if(quantidade_ciclos % ratio_coordenadas == 0):
                 if (j_alturas < (len(alturas[i])) and len(alturas[i]) > 1):
                     media_alturas = (alturas[i][j_alturas] - alturas[i][j_alturas - 1]) / ratio_coordenadas
+
                     flag = 1
                     j_alturas += 1
             else:
                 if (j == 0 and len(alturas[i]) >= 2):
                     media_alturas = (alturas[i][j_alturas] - alturas[i][j_alturas - 1]) / (ratio_coordenadas - 1)
                     matriz_gerada[i][j] = alturas[i][j_alturas - 1] - (media_alturas * (ratio_coordenadas - 1))
+                    if (matriz_gerada[i][j] < 0):
+                        matriz_gerada[i][j] = 0
                     flag = 0
                     quantidade_ciclos += 1
+                    j += 1
                     continue
                 if(flag == 1):
                     matriz_gerada[i][j] = alturas[i][j_alturas - 2] + media_alturas
+                    if(matriz_gerada[i][j] < 0):
+                        matriz_gerada[i][j] = 0
                     flag = 0
                 else:
                     matriz_gerada[i][j] = matriz_gerada[i][j-1] + media_alturas
+                    if (matriz_gerada[i][j] < 0):
+                        matriz_gerada[i][j] = 0
 
+            j += 1
             quantidade_ciclos += 1
+        print "mudou de linha"
+        i += 1
 
 
 
+print "Gravando matriz"
 
 matriz_convertida = np.array(matriz_gerada)
 matrix = np.matrix(matriz_convertida)
